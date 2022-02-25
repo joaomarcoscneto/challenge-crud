8a1750d90b15057f86fd1bf7a97bc8253fe415f8
João Marcos <joaomarcoscneto@gmail.com>
2022-02-25 01:35:31 -0300

    feat: add route store users, validations and tests

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
         return response()->json(['users' => User::all()]);
+    }
+
+    public function store(UserRequest $request)
+    {
+        $data = $request->validated();
+        $data['password']  = Hash::make($data['password']);
+
+        return response()->json(['user' => User::create($data)], 201);
     }
 }
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -37,4 +37,142 @@ class UserTest extends TestCase
 
         $this->assertDatabaseCount('users', 2);
     }
+
+    public function testStore()
+    {
+        $user = User::factory()->make();
+
+        $userArray = $user->toArray();
+        $userArray['password'] = '123456';
+        $userArray['password_confirmation'] = '123456';
+
+        $response = $this->postJson('/users', $userArray);
+
+        $response->assertStatus(201)
+            ->assertJsonStructure([
+                'user' => [
+                    'name',
+                    'email',
+                    'city',
+                    'state',
+                    'created_at',
+                    'updated_at',
+                    'id'
+                ]
+            ]);
+
+        $this->assertDatabaseCount('users', 1);
+    }
+
+    public function testStoreValidationRequired()
+    {
+        $user = User::factory()->make([
+            'name' => '',
+            'email' => '',
+            'city' => '',
+            'state' => '',
+        ]);
+
+        $userArray = $user->toArray();
+        $userArray['password'] = '';
+        $userArray['password_confirmation'] = '';
+
+        $response = $this->postJson('/users', $userArray);
+
+        $response->assertInvalid(['name', 'email', 'city', 'state', 'password']);
+    }
+
+    public function testStoreValidationTypeFields()
+    {
+        $user = User::factory()->make([
+            'name' => 1010,
+            'email' => 1010,
+            'city' => 1010,
+            'state' => 1010,
+        ]);
+
+        $userArray = $user->toArray();
+        $userArray['password'] = 010101;
+        $userArray['password_confirmation'] = 010101;
+
+        $response = $this->postJson('/users', $userArray);
+
+        $response->assertStatus(422)
+            ->assertInvalid(['name', 'email', 'city', 'state', 'password']);
+    }
+
+    public function testStoreValidationEmail()
+    {
+        $user = User::factory()->make([
+            'email' => 'sdafsdf'
+        ]);
+
+        $userArray = $user->toArray();
+        $userArray['password'] = '123456';
+        $userArray['password_confirmation'] = '123456';
+
+        $response = $this->postJson('/users', $userArray);
+
+        $response->assertStatus(422)
+            ->assertInvalid(['email']);
+    }
+
+    public function testStoreValidationMinLength()
+    {
+        $user = User::factory()->make();
+
+        $userArray = $user->toArray();
+        $userArray['password'] = '0101';
+        $userArray['password_confirmation'] = '0101';
+
+        $response = $this->postJson('/users', $userArray);
+
+        $response->assertStatus(422)
+            ->assertInvalid(['password']);
+    }
+
+    public function testStoreValidationWithoutPasswordConfirmation()
+    {
+        $user = User::factory()->make();
+
+        $userArray = $user->toArray();
+        $userArray['password'] = '123456';
+
+        $response = $this->postJson('/users', $userArray);
+
+        $response->assertStatus(422)
+            ->assertInvalid(['password']);
+    }
+
+    public function testStoreValidationWithDiferentPasswordConfirmation()
+    {
+        $user = User::factory()->make();
+
+        $userArray = $user->toArray();
+        $userArray['password'] = '123456';
+        $userArray['password_confirmation'] = '010101';
+
+        $response = $this->postJson('/users', $userArray);
+
+        $response->assertStatus(422)
+            ->assertInvalid(['password']);
+    }
+
+    public function testStoreUniqueEmailValidation()
+    {
+        $user1 = User::factory()->create();
+
+        $user2 = User::factory()->make([
+            'email' => $user1->email
+        ]);
+
+        $userArray = $user2->toArray();
+        $userArray['password'] = '123456';
+        $userArray['password_confirmation'] = '010101';
+
+        $response = $this->postJson('/users', $userArray);
+
+        $response->assertStatus(422)
+            ->assertInvalid(['email']);
+    }
 }
