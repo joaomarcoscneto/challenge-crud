f097fc090bd24856aeeebafe9d1c4f387a77bf33
João Marcos <joaomarcoscneto@gmail.com>
2022-02-25 12:09:25 -0300

    fix: change permissions

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="card">
-    <div class="card-header">E-Commerce Brasil</div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">CRUD Users</div>
 
-    <div class="card-body">
-      <example-component></example-component>
+          <div class="card-body">
+            <input
+              v-model="form.email"
+              type="text"
+              placeholder="Email"
+              class="form-control"
+              :class="{ 'is-invalid': form.errors.has('email') }"
+              name="email"
+            />
+
+            <input
+              v-model="form.password"
+              type="text"
+              placeholder="password"
+              class="form-control"
+              :class="{ 'is-invalid': form.errors.has('password') }"
+              name="password"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import ExampleComponent from "./components/ExampleComponent.vue";
-
 export default {
-  components: { ExampleComponent },
   mounted() {
     console.log("Component mounted.");
   },
